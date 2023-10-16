11939218d012ba062112fa8f3f821b04f75967e8
Andromeda <andromeda951@gmail.com>
2023-10-16 15:23:11 +0700

    Add icon, title, and options menu in action bar home activity

--- app/src/main/java/com/andromeda/latihanuts/HomeActivity.kt
+++ app/src/main/java/com/andromeda/latihanuts/HomeActivity.kt
@@ -2,10 +2,26 @@ package com.andromeda.latihanuts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.ActionBar
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val actionBar: ActionBar? = supportActionBar
+
+        actionBar?.title = "UNION"
+        actionBar?.setIcon(R.drawable.app_logo)
+
+        actionBar?.setDisplayUseLogoEnabled(true)
+        actionBar?.setDisplayShowHomeEnabled(true)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }

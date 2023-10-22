625d6dfb40894a35f32df21be30d97a6c1dd9c23
Andromeda <andromeda951@gmail.com>
2023-10-22 14:27:26 +0700

    Add plugin kotlin-parcelize and update data class MovieModel

--- app/src/main/java/com/andromeda/latihanuts/MovieModel.kt
+++ app/src/main/java/com/andromeda/latihanuts/MovieModel.kt
@@ -1,8 +1,9 @@
 package com.andromeda.latihanuts
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class MovieModel(
     val title: String?,
     val description: String?,
@@ -10,32 +11,4 @@ data class MovieModel(
     val genre: List<String>?,
     val year: Int,
     val rating: String?,
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.createStringArrayList(),
-        parcel.readInt(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun describeContents(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun writeToParcel(p0: Parcel, p1: Int) {
-        TODO("Not yet implemented")
-    }
-
-    companion object CREATOR : Parcelable.Creator<MovieModel> {
-        override fun createFromParcel(parcel: Parcel): MovieModel {
-            return MovieModel(parcel)
-        }
-
-        override fun newArray(size: Int): Array<MovieModel?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
